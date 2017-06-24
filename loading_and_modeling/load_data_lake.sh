fd7793050c975aed41b713c8fd8a1e9c072fb826
mikegruzynski <mike.gruzynski@berkeley.edu>
2017-06-24 12:23:38 -0700

    Create load_data_lake.sh

---
+++ loading_and_modeling/load_data_lake.sh
@@ -0,0 +1,11 @@
+tail -n +2 effective_care.csv > effective_care_noH.csv
+tail -n +2 hospitals.csv > hospitals_noH.csv
+tail -n +2 measures.csv > measures_noH.csv
+tail -n +2 readmissions.csv > readmissions_noH.csv
+tail -n +2 surveys_responses.csv > surveys_responses_noH.csv
+
+hdfs dfs -put ./effective_care_noH.csv /user/w205/hospital_compare
+hdfs dfs -put ./hospitals_noH.csv /user/w205/hospital_compare
+hdfs dfs -put ./measures_noH.csv /user/w205/hospital_compare
+hdfs dfs -put ./readmissions_noH.csv /user/w205/hospital_compare
+hdfs dfs -put ./surveys_responses_noH.csv /user/w205/hospital_compare
