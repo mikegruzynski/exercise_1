8ad25788f5b8560de2dc70fdd56f845db00a0fda
mikegruzynski <mike.gruzynski@berkeley.edu>
2017-06-24 23:20:05 -0700

    Update load_data_lake.sh

--- loading_and_modeling/load_data_lake.sh
+++ loading_and_modeling/load_data_lake.sh
@@ -1,19 +1,20 @@
-# Files where already on /root/data from scp command from download file. 
-# I already changed the name of the files on the PC before pushing them to AWS instance.
-# I was in the directory of the /root/data/ directory where the csv files lived when performing commands
+# transfer the file from PC onto AWS EC2 instance
+scp -i "UCBW205.pem" /cygdrive/c/Users/mgruz/Desktop/exercise_1/data/*.csv  root@ec2-34-204-3-76.compute-1.amazonaws.com:/data/
+# change directory into where I put raw data
+cd /data/
+# make hadoop directory, one folder per csv file
 hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
 hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
 hdfs dfs -mkdir /user/w205/hospital_compare/measures
 hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
 hdfs dfs -mkdir /user/w205/hospital_compare/surveys_responses
-
-
+# remove the header from all the csv file so SQL data creation 
 tail -n +2 effective_care.csv > effective_care_noH.csv
 tail -n +2 hospitals.csv > hospitals_noH.csv
 tail -n +2 measures.csv > measures_noH.csv
 tail -n +2 readmissions.csv > readmissions_noH.csv
 tail -n +2 surveys_responses.csv > surveys_responses_noH.csv
-
+# hdfs (hadoop) commands to place files in newly created folders
 hdfs dfs -put ./effective_care_noH.csv /user/w205/hospital_compare/effective_care
 hdfs dfs -put ./hospitals_noH.csv /user/w205/hospital_compare/hospitals
 hdfs dfs -put ./measures_noH.csv /user/w205/hospital_compare/measures
