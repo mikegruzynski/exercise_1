191c70fbafe48d45be9318fe16d256613eb07c4b
mikegruzynski <mike.gruzynski@berkeley.edu>
2017-07-07 15:31:23 -0700

    Update spl.sql

--- investigations/best_hospitals/spl.sql
+++ investigations/best_hospitals/spl.sql
@@ -71,49 +71,49 @@ FROM bh_eff_ed_avg df;
 -- 
 DROP TABLE bh_eff_hacp_rank;
 CREATE TABLE bh_eff_hacp_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_hacp_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_hacp_avg DESC) as bh_eff_avg_hacp_rank,
        df.hospital_name,df.bh_eff_hacp_avg
 FROM bh_eff_hacp_avg df;
 -- 
 DROP TABLE bh_eff_hf_rank;
 CREATE TABLE bh_eff_hf_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_hf_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_hf_avg DESC) as bh_eff_avg_hf_rank,
        df.hospital_name,df.bh_eff_hf_avg
 FROM bh_eff_hf_avg df;
 -- 
 DROP TABLE bh_eff_pc_rank;
 CREATE TABLE bh_eff_pc_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_pc_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_pc_avg DESC) as bh_eff_avg_pc_rank,
        df.hospital_name,df.bh_eff_pc_avg
 FROM bh_eff_pc_avg df;
 -- 
 DROP TABLE bh_eff_scip_rank;
 CREATE TABLE bh_eff_scip_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_scip_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_scip_avg DESC) as bh_eff_avg_scip_rank,
        df.hospital_name,df.bh_eff_scip_avg
 FROM bh_eff_scip_avg df;
 -- 
 DROP TABLE bh_eff_pdc_rank;
 CREATE TABLE bh_eff_pdc_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_pdc_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_pdc_avg DESC) as bh_eff_avg_pdc_rank,
        df.hospital_name,df.bh_eff_pdc_avg
 FROM bh_eff_pdc_avg df;
 -- 
 DROP TABLE bh_eff_p_rank;
 CREATE TABLE bh_eff_p_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_p_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_p_avg DESC) as bh_eff_avg_p_rank,
        df.hospital_name,df.bh_eff_p_avg
 FROM bh_eff_p_avg df;
 -- 
 DROP TABLE bh_eff_sc_rank;
 CREATE TABLE bh_eff_sc_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_sc_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_sc_avg DESC) as bh_eff_avg_sc_rank,
        df.hospital_name,df.bh_eff_sc_avg
 FROM bh_eff_sc_avg df;
 -- 
 DROP TABLE bh_eff_bcptca_rank;
 CREATE TABLE bh_eff_bcptca_rank AS
-SELECT RANK() OVER(ORDER BY df.bh_eff_bcptca_avg DESC) as bh_eff_avg_ed_rank,
+SELECT RANK() OVER(ORDER BY df.bh_eff_bcptca_avg DESC) as bh_eff_avg_bcptca_rank,
        df.hospital_name,df.bh_eff_bcptca_avg
 FROM bh_eff_bcptca_avg df;
 --
