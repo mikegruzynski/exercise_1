e3c0a9ab4b092d953062f5dd80b47903fb200536
mikegruzynski <mike.gruzynski@berkeley.edu>
2017-07-09 12:12:34 -0700

    Update best_states.sql

--- investigations/best_states/best_states.sql
+++ investigations/best_states/best_states.sql
@@ -1,4 +1,6 @@
---
+-- grab all of the filtered data from best_hospital_ranks (information in transformation.sql comments.
+-- Only will consider hospitals with all 9 conditions in the table, and group by state and average each state.
+-- Lower rank is better, and display in order from smallest to largest.
 SELECT state, round((avg(bh_eff_avg_ed_rank) + avg(bh_eff_avg_hacp_rank) + avg(bh_eff_avg_hf_rank) + avg(bh_eff_avg_pc_rank) + avg(bh_eff_avg_scip_rank) + avg(bh_eff_avg_pdc_rank) + avg(bh_eff_avg_p_rank) + avg(bh_eff_avg_sc_rank) + avg(bh_eff_avg_bcptca_rank))/ 9.0, 2) as state_rank_avg
 FROM best_hospital_ranks
 GROUP BY state
